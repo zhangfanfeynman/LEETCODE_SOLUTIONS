89f0c9fca4f80d55da13556fd71c59d24bbca487
zhangfanfeynman <fanz8905@gmail.com>
2026-01-17 14:30:27 +0800

    update 0116

--- leetcode_hot_100/solutions.py
+++ leetcode_hot_100/solutions.py
@@ -810,6 +810,29 @@ def trap(height:List[int])->int:
             water_trapped += left_max - height[left]
         else:
             right -=1
-            right_max = max(right_max, height[right]])
-            water_trapped += right_max - height[i]
+            right_max = max(right_max, height[right])
+            water_trapped += right_max - height[right]
     return water_trapped
+
+
+# leetcode 39. 组合总和
+# 给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 
+# target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+# candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。
+
+def combinationSum(candidates:List[int], target:int):
+    def backtrack(remaining, combo, start):
+        if remaining == 0:
+            result.append(combo[:])
+            return
+        elif remaining < 0 :
+            return
+        for i in range(start, len(candidates)):
+            combo.append(candidates[i])
+            backtrack(remaining - candidates[i], combo, i)
+            combo.pop()
+    result = []
+    backtrack(target, [], 0)
+    return result
+
+
