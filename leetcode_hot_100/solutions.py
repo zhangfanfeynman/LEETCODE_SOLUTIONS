73f764b428e2b13244857cf515fcd149b17be5db
zhangfanfeynman <fanz8905@126.com>
2026-01-25 21:00:47 +0800

    update 0125-1 fix bug

--- leetcode_hot_100/solutions.py
+++ leetcode_hot_100/solutions.py
@@ -1079,7 +1079,7 @@ def threeSum(nums:List[int])->List[List[int]]:
                 right -=1
             else:
                 result.append([nums[i], nums[left], nums[right]])
-                while left<right and nums[left] == nums[left + 1]
+                while left<right and nums[left] == nums[left + 1]:
                     left +=1
                 while left<right and nums[right] == nums[right-1]:
                     right -=1
