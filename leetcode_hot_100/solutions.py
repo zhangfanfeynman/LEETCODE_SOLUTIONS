2c0419d838faab3deeba6528933d6957edaa6d49
zhangfanfeynman <fanz8905@126.com>
2026-01-25 20:56:48 +0800

    update 0125-1

--- leetcode_hot_100/solutions.py
+++ leetcode_hot_100/solutions.py
@@ -1058,3 +1058,36 @@ def removeNthFromEnd(head:ListNode)->ListNode:
     slow.next = slow.next.next
     return dummy.next
 
+# leetcode 15. 三数之和
+# 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+
+# 注意：答案中不可以包含重复的三元组。
+
+def threeSum(nums:List[int])->List[List[int]]:
+    nums.sort()
+    result = []
+    n = len(nums)
+    for i in range(n-2):
+        if i>0 and nums[i] == nums[i-1]:
+            continue
+        left, right = i+1, n-1
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+            if total<0:
+                left +=1
+            elif total>0:
+                right -=1
+            else:
+                result.append([nums[i], nums[left], nums[right]])
+                while left<right and nums[left] == nums[left + 1]
+                    left +=1
+                while left<right and nums[right] == nums[right-1]:
+                    right -=1
+                left +=1
+                right -=1
+
+    return result
+
+
+
+
