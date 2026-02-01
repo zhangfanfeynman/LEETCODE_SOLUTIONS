cb85c4d2fd5a9da321700d4f01ed263452a43030
zhangfanfeynman <fanz8905@126.com>
2026-02-01 18:08:57 +0800

    update 0201-3

--- leetcode_hot_100/solutions.py
+++ leetcode_hot_100/solutions.py
@@ -1171,6 +1171,31 @@ def addTwoNumbers(l1:ListNode, l2:ListNode)->ListNode:
         if l1:
             l1 = l1.next
         if l2:
-            l2 = le.next
+            l2 = l2.next
     return dummy.next
 
+#### leetcode 79. 单词搜索
+# 给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
+
+# 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+
+def exist(board:List[List[str]], word:str)->bool:
+    
+    def dfs(i, j, index):
+        if index == len(word):
+            return True
+        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or board[i][j] != word[index]:
+            return False
+        temp = board[i][j]
+        board[i][j] = '#'
+        found = (dfs(i+1, j, index+1) or
+                 dfs(i-1, j, index+1) or
+                 dfs(i, j+1, index+1) or
+                 dfs(i, j-1, index+1))
+        board[i][j] = temp
+        return found
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if dfs(i,j,0):
+                return True
+    return False
