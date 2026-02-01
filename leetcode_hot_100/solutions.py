cfc1d822be135c89c5662af8c36f5a38f1330b25
zhangfanfeynman <fanz8905@126.com>
2026-02-01 17:57:00 +0800

    update 0201-2

--- leetcode_hot_100/solutions.py
+++ leetcode_hot_100/solutions.py
@@ -1150,4 +1150,27 @@ def lengthOfLongestSubstring(s:str)->int:
         max_length = max(max_length, right - left + 1)
     return max_length
 
+# leetcode 2. 两数相加
+
+# 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+# 请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+def addTwoNumbers(l1:ListNode, l2:ListNode)->ListNode:
+    dummy = ListNode(0)
+    current = dummy
+    carry = 0
+    while l1 or l2 or carry:
+        val1 = l1.val if l1 else 0
+        val2 = l2.val if l2 else 0
+        total = val1 + val2 + carry
+        carry = total//10
+        digit = total%10
+        current.next = ListNode(digit)
+        current = current.next
+        if l1:
+            l1 = l1.next
+        if l2:
+            l2 = le.next
+    return dummy.next
 
